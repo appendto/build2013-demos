d5472929ad332f2da968ea8011e6209c8e9b8d17
Jonathan Sampson <jjdsampson@gmail.com>
2013-06-27 22:54:59 -0400

    Added Sampson's Demo

---
+++ sampson/StackerTracker.R.Ko/StackerTracker.R.Ko/js/app.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0/js/ui.js" />
+/// <reference path="//Microsoft.WinJS.1.0/js/base.js" />
+require.config({
+    paths: {
+        jquery: "../Scripts/jquery-2.0.2",
+        ko: "../Scripts/knockout-2.2.1",
+        users: "modules/users"
+    }
+});
+
+require(["jquery", "ko", "users"], function ($, ko, users) {
+
+    var ids = [6380, 1535478, 405892, 54680, 5];
+
+    users.getUsers(ids);
+
+    setInterval(function () {
+        $.each(users.users(), function (index, user) {
+            user.reputation(5 + user.reputation());
+            user.answer_count(1 + user.answer_count());
+        });
+    }, 3000);
+
+    var App = function () {
+        this.users = users.users;
+    }
+
+    $(document).ready(function () {
+        ko.applyBindings(new App());
+    });
+
+});
---
+++ sampson/StackerTracker.R.Ko/StackerTracker.R.Ko/js/modules/users.js
@@ -0,0 +1,31 @@
+﻿define(["jquery", "ko"], function ($, ko) {
+
+    "use strict";
+
+    var user_list = ko.observableArray();
+    var observableProps = ["reputation", "answer_count"];
+
+    function retrieveUsers(ids) {
+        $.ajax({
+            url: "http://api.stackoverflow.com/1.1/users/" + ids.join(";"),
+            success: function (response) {
+                $.each(response.users, function (index, user) {
+                    addObservables(user, observableProps);
+                    user_list.push(user);
+                });
+            }
+        });
+    }
+
+    function addObservables(obj, props) {
+        props.forEach(function (prop) {
+            obj[prop] = ko.observable(obj[prop]);
+        });
+    }
+
+    return {
+        users: user_list,
+        getUsers: retrieveUsers
+    }
+
+});
